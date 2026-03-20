43ffbd16875b0d87ec2b69a4adfff4725499996b
Ido Naor <idonaor15.do@gmail.com>
2026-03-20 02:18:29 +0200

    Remove user info fields from Review and Comment models and bump database version

--- app/src/main/java/com/tasteclub/app/data/model/Review.kt
+++ app/src/main/java/com/tasteclub/app/data/model/Review.kt
@@ -3,8 +3,6 @@ package com.tasteclub.app.data.model
 data class Review(
     val id: String = "",
     val userId: String = "",
-    val userName: String = "",
-    val userProfileImageUrl: String = "",
 
     val restaurantId: String = "",
     val restaurantName: String = "",
